3ebb12dd4bcf686710c0263346c92ba8001b7e30
Rashi Bhanushali <rashibhanushali45@gmail.com>
2024-09-16 12:26:15 +0530

    Initial commit

---
+++ components/Resume.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import MagicButton from './ui/MagicButton';
+import { PiReadCvLogo } from 'react-icons/pi';
+import { navItems, socialMedia } from '@/data';
+import { FaLocationArrow } from 'react-icons/fa6';
+
+const Resume = () => {
+    const resumeItem = navItems.find(item => item.name === "Resume");
+
+    return (
+        <div className="flex flex-col items-center relative my-20 z-10 mt-0">
+            <div className="flex justify-between w-full max-w-md gap-8"> {/* Added gap-8 for space between buttons */}
+                {resumeItem && (
+                    <a href={resumeItem.link} target={resumeItem.target} rel={resumeItem.rel}>
+                        <MagicButton
+                            title="Here&apos;s my Resume"
+                            icon={<PiReadCvLogo />}
+                            position='right'
+                        />
+                    </a>
+                )}
+                <a href="mailto:rashibhanushali45@gmail.com">
+                    <MagicButton
+                        title="Contact me!"
+                        icon={<FaLocationArrow />}
+                        position='right'
+                    />
+                </a>
+            </div>
+            
+            <footer className="w-full mt-20">
+                <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+                    <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 Rashi</p>
+                    <div className='flex items-center md:gap-3 gap-6'>
+                        {socialMedia.map((profile) => (
+                            <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-200'>
+                                <img src={profile.img} alt={profile.id} width={20} height={20} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Resume;
---
+++ data/index.ts
@@ -0,0 +1,119 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Resume", 
+      link: "/Rashi_Bhanushali_Resume.pdf",
+      target: "_blank",
+      rel: "noopener noreferrer"
+    },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Innovative fresher with a growing foundation in web development.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Always exploring new 3D animations to enhance user experiences.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Passionate about developing user-centric applications.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently working on an E-Books E-Commerce Website",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Amazon Clone",
+      des: "Replica of Amazon Global Home Page using HTML and CSS.",
+      img: "/p1.svg",
+      iconLists: ["/html.svg", "/css.svg"],
+      link: "https://github.com/rashiibhanushali/Amazon-Clone",
+    },
+    {
+      id: 2,
+      title: "CRUD using ReactJs",
+      des: "A web application that allows users to create, read, update and delete using ReactJS with context API.",
+      img: "/p2.svg",
+      iconLists: ["/react.svg"],
+      link: "https://github.com/rashiibhanushali/CRUD-APP",
+    },
+    {
+      id: 3,
+      title: "To Do Web App",
+      des: "A User-Friendly To-Do Web based App to manage the daily tasks.",
+      img: "/p3.svg",
+      iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+      link: "https://github.com/rashiibhanushali/oibsip_taskno3",
+    },
+    {
+      id: 4,
+      title: "Simple Calculator",
+      des: "A simple Basic Calculator with unique UI",
+      img: "/p4.svg",
+      iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+      link: "https://github.com/rashiibhanushali/oibsip_taskno1",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
